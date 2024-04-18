889577474670fe96ad66a4991d01d3c18c33dd93
SONPD <wisdom231300@gmail.com>
2024-04-19 00:55:35 +0700

    PlguinsManager

--- Editor/PluginManager/PluginConfig.cs
+++ Editor/PluginManager/PluginConfig.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PluginConfig", menuName = "Configuration/PluginConfig")]
 public class PluginConfig : ScriptableObject
 {
-    public PluginData[] plugins;
+    public List<PluginData> plugins = new List<PluginData>();
 }
 
 [System.Serializable]
@@ -12,6 +13,23 @@ public class PluginData
     public ePlugin Name;
     public bool IsEnable;
     public string DefineSymbol;
+    public PluginData()
+    {
+        Name = ePlugin.None;
+        IsEnable = false;
+        DefineSymbol = $"USE_{Name.ToString().ToUpper()}";
+    }
+
+    // Change DefineSymbol when Name is changed
+    public void ChangeDefineSymbol()
+    {
+        if(Name == ePlugin.None)
+        {
+            DefineSymbol = "";
+            return;
+        }
+        DefineSymbol = $"USE_{Name.ToString().ToUpper()}";
+    }
 }
 public enum ePlugin
 {
