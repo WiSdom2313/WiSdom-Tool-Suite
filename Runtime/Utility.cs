ffb058396c3caf910a80ced8c19b7f2b37bb6382
WiSdom <wisdom230200@gmail.com>
2024-06-08 08:07:57 +0700

    plugin

--- Runtime/Utility.cs
+++ Runtime/Utility.cs
@@ -7,7 +7,7 @@ namespace WiSdom
     public static class Utility
     {
         // Helper method to generate a more secure file name
-        public static string EncodeStringToByte(string input)
+        public static string EncodeStringToByteString(string input)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(input);
             string encodedString = Convert.ToBase64String(bytes);
@@ -16,13 +16,24 @@ namespace WiSdom
         }
 
         // Convert a Base64 string back to a normal string
-        public static string DecodeByteFromString(string input)
+        public static string DecodeStringByteFromString(string input)
         {
             Debug.Log("DecodeByteFromString: " + input);
             byte[] bytes = Convert.FromBase64String(input);
             string decodedString = Encoding.UTF8.GetString(bytes);
 
             return decodedString;
+        }
+
+        public static byte[] DecodeByteFromString(string input)
+        {
+            byte[] bytes = Convert.FromBase64String(input);
+            return bytes;
+        }
+        public static string EncodeByteToString(byte[] input)
+        {
+            string encodedString = Convert.ToBase64String(input);
+            return encodedString;
         }
     }
 }
